6b71b630f81c96c1355cc8238af155e18c0666e2
marvin <marvin.kuehne32@gmail.com>
2025-03-31 17:49:52 +0200

    added another route

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+counter = 0
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    global counter
+    counter = counter + 1
+    return 'Hello World! ' + str(counter)
 
+@app.route('/hello')
+def hello_world1():  # put application's code here
+    return 'Hello World 2!'
 
 if __name__ == '__main__':
     app.run()
